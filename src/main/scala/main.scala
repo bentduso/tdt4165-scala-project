16724d086bbb937c274fdc2c0a2163e3464cd13e
Benjamin Zubača <bzubaca123@gmail.com>
2024-10-07 11:03:07 +0200

    Complete task 1 in part 1

--- src/main/scala/main.scala
+++
@@ -1,4 +0,0 @@
-package no.ntnu.tdt4165
-
-@main
-def main(): Unit = println("Hello, world!")
---
+++ src/main/scala/part1/Task1.scala
@@ -0,0 +1,27 @@
+package no.ntnu.tdt4165
+package part1
+
+object Task1 {
+  def generateArray(): Array[Int] = {
+    val arr = new Array[Int](50)
+    for i <- 1 to 50 do arr(i - 1) = i
+    arr
+  }
+
+  def sum(arr: Array[Int]): Int = {
+    var sum = 0
+    for num <- arr do sum += num
+    sum
+  }
+
+  def recursiveSum(arr: Array[Int]): Int = arr match {
+    case Array() => 0
+    case Array(head, tail*) => head + recursiveSum(tail.toArray)
+  }
+
+  def fibonacci(n: BigInt): BigInt = n match {
+    case n if n == BigInt(0) => 0
+    case n if n == BigInt(1) => 1
+    case _ => fibonacci(n - 1) + fibonacci(n - 2)
+  }
+}
