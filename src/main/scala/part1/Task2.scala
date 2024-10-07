8fc7f0495fb40f1595b2f9849aaf4d5af095a041
Benjamin Zubača <bzubaca123@gmail.com>
2024-10-07 11:22:00 +0200

    Complete task 2 in part 1

---
+++ src/main/scala/part1/Task2.scala
@@ -0,0 +1,19 @@
+package no.ntnu.tdt4165
+package part1
+
+object Task2 {
+  def quadraticEquation(a: Float, b: Float, c: Float): (Boolean, Option[Double], Option[Double]) = {
+    val discriminant = b * b - 4.0 * a * c
+
+    if (discriminant >= 0) {
+      val x1 = (-b + math.sqrt(discriminant)) / (2.0 * a)
+      val x2 = (-b - math.sqrt(discriminant)) / (2.0 * a)
+      (true, Some(x1), Some(x2))
+    } else {
+      (false, None, None)
+    }
+  }
+
+  def quadratic(a: Float, b: Float, c: Float): Float => Float =
+    (x: Float) => a * x * x + b * x + c
+}
